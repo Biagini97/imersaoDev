fadea7057241a7a6519a7908ead9a47a2c89c835
Biagini97 <luizbiagini97@gmail.com>
2021-09-29 23:16:15 -0300

    Caculadora de média finalizada

--- Calculadora de media/main.js
+++ Calculadora de media/main.js
@@ -5,12 +5,14 @@ function Calcular()
     grades += parseFloat((document.getElementById("grade3")).value);
     grades += parseFloat((document.getElementById("grade4")).value);
     grades = grades/4
-    var resultado = document.getElementById("resultado").innerHTML;
-    document.getElementById("resultado").innerHTML = "<h2>"+grades+"</h2>";
-    console.log(resultado);
+    var resultado =document.getElementById("resultado").innerHTML;
+    document.getElementById("resultado").innerHTML ="A sua média é " +grades;
+
+    
+} 
 //     console.log(document.getElementById("grade2").value);
 //     console.log(document.getElementById("grade3").value);
 //     console.log(document.getElementById("grade4").value);
 //     console.log(document.getElementById("grade4").value);
-// }
-}
+// 
+
